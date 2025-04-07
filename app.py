ac5f4f7032d5a56601efb2001f708f7a40005300
FRUSTATED Engineer <149618147+hacker404bot@users.noreply.github.com>
2025-04-07 16:29:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return "Hello from Flask!"
+
+
 
 # Braille dictionary mapping text to Unicode Braille patterns
 braille_dict = {
